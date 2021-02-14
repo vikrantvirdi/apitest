54e4c1d814da940442090220fc35d10ee215e630
Vikrant <vikrantvirdi808@gmail.com>
2021-02-14 19:57:55 +0530

    first commit

---
+++ Template-engine/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path =require('path');
+const app = express();
+const port =8000;
+//build in middleware
+const staticPath =path.join(__dirname,"public");
+app.use(express.static(staticPath));
+
+app.get("/",(req,res)=>{
+    res.send("helloo");
+});
+app.listen(port,()=>{
+    console.log(`listeninng `)
+});
---
+++ app.js
@@ -0,0 +1,16 @@
+var http = require('http');
+const kelvinToCelsius = require('kelvin-to-celsius');
+var url ='https://api.openweathermap.org/data/2.5/weather?q=dasuya&units=metric&lang=hi&appid=d988fea20c02ea8882beb28367c7c3ff';
+var server =http.createServer(function(request,response){
+var request =require('request');
+request(url,function(err,res,body){
+    var data=JSON.parse(body);
+    const cel = `${data.main['temp']}`-273.15 ;
+    response.write("<html><body><div id='container'>");
+    response.write("<h1>"+ 'City Name:'+ data['name']+'<br>'+'</h1>');
+    response.write("<h2>"+ 'Temperature:'+ data.main['temp']+'<br>wind:'+ data.main['feels_like']+'<br>'+'</h1>');
+    response.write("</div></body></html>");
+    response.end();
+});
+
+}).listen(3000);
