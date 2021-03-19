9d72f91b8bcc9b59cddeddf49e14e474513b4d56
vikrantvirdi <vikrantvirdi808@gmail.com>
2021-03-19 22:44:45 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const kelvinToCelsius = require('kelvin-to-celsius');
 var url ='https://api.openweathermap.org/data/2.5/weather?q=dasuya&units=metric&lang=hi&appid=d988fea20c02ea8882beb28367c7c3ff';
 var server =http.createServer(function(request,response){
 var request =require('request');
+    var re = require ('grunt');
 request(url,function(err,res,body){
     var data=JSON.parse(body);
     const cel = `${data.main['temp']}`-273.15 ;
